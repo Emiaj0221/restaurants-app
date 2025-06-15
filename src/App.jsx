f1d167050a1845398b3febc32751abfec13ddd66
lgiraldo-dev <lgiraldo@quanspire.com>
2025-06-15 11:43:51 -0500

    Ajuste al Browser router, para que github pages no nos falle en el redireccionamiento de las rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Home from './pages/Home';
 import NewRestaurant from './pages/NewRestaurant';
